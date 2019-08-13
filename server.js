ef105c6f1e921cbe0c1812f35e4ea37fdf2a9112
nabendu82 <nabendu.biswas@gmail.com>
2019-08-13 19:26:35 +0530

    Heroku deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const connectDB = require('./config/db');
 
@@ -14,6 +15,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/employees', require('./routes/api/employees'));
 app.use('/api/hospitals', require('./routes/api/hospitals'));
 app.use('/api/patients', require('./routes/api/patients'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
